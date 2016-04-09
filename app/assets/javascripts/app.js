88756ece8871136fd91f8007cf6a5904b1cbb179
Alaric Calmette <alaric.calmette@gmail.com>
2016-04-09 13:46:25 +0200

    rewrite repo

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,30 @@
+angular.module('fafMtp', ['ui.router', 'templates'])
+	.config([
+		'$stateProvider',
+		'$urlRouterProvider',
+		function($stateProvider, $urlRouterProvider) {
+
+			$stateProvider
+				.state('home', {
+					url: '/home',
+					templateUrl: 'home/_home.html',
+					controller: 'MainController',
+					resolve: {
+						postPromise: ['posts', function(posts){
+							return posts.getAll();
+						}]
+					}
+				})
+				.state('posts', {
+					url: '/posts/{id}',
+					templateUrl: 'posts/_posts.html',
+					controller: 'PostsController',
+					resolve: {
+						post: ['$stateParams', 'posts', function($stateParams, posts) {
+							return posts.get($stateParams.id);
+						}]
+					}
+				});
+
+			$urlRouterProvider.otherwise('home');
+		}]);
